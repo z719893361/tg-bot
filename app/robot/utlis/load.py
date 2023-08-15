77a652ca2c0b923dd4f4012002492c88634644fc
格索伊 <719893361@qq.com>
2023-08-15 17:38:56 +0800

    新增扫包方法

--- app/robot/utlis/load.py
+++ app/robot/utlis/load.py
@@ -17,3 +17,15 @@ def load_classes_from_directory(directory: Path, base_class: Type[T]) -> List[Ty
         for name, cls in inspect.getmembers(module, inspect.isclass):
             if issubclass(cls, base_class) and cls != base_class:
                 yield cls
+
+
+def scan_and_load(directory: Path, current):
+    for path in directory.glob('*'):
+        if path == current:
+            continue
+        if path.match('*.py'):
+            spec = spec_from_file_location(__name__, path)
+            module = module_from_spec(spec)
+            spec.loader.exec_module(module)
+        if path.is_dir():
+            scan_and_load(path, current)
